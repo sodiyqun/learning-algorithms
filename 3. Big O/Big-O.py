87e070239938cc0a38674d6d3887335ca677935a
sodiyqun <94372756+sodiyqun@users.noreply.github.com>
2022-01-05 23:45:34 +0500

    add

---
+++ 3. Big O/Big-O.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  5 23:42:34 2022
+
+@author: sodiyqun
+"""
+
+# Big O
+
+# Array Big O:
+# read = O(1)
+# add = O(n)
+# delete = O(n)
+
+# LinkedLists Big O:
+# read = O(n)
+# add = O(1)
+# delete = O(1)
